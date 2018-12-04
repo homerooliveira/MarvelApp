311aa2f142e261086fed24646f1ba1d74f46f487
Homero Oliveira <homero.santos.001@acad.pucrs.br>
2018-12-04 20:23:30 -0200

    created viewModel and ViewController

---
+++ MarvelApp/MarvelApp/ListCharacters/ListCharactersViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ListCharactersViewController.swift
+//  MarvelApp
+//
+//  Created by Homero Oliveira on 04/12/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import UIKit
+
+final class ListCharactersViewController: UIViewController {
+    let viewModel: ListCharactersViewModel
+    
+    init(viewModel: ListCharactersViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ MarvelApp/MarvelApp/ListCharacters/ListCharactersViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListCharactersViewModel.swift
+//  MarvelApp
+//
+//  Created by Homero Oliveira on 04/12/18.
+//  Copyright © 2018 Homero Oliveira. All rights reserved.
+//
+
+import Foundation
+
+final class ListCharactersViewModel {
+    
+}
